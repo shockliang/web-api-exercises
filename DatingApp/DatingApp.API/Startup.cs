a2513066a8fffd26292a577e991cc3e7061b57c0
Shock Liang <shockwater@gmail.com>
2017-12-21 23:44:32 +0800

    Registering services in the Startup class

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
             });
             services.AddMvc();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRespository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
