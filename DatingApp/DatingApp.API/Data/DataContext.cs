2e4754c137a991cb6bedcc6b2d783b5918d1e8f1
Shock Liang <shockwater@gmail.com>
2017-12-21 22:29:03 +0800

    Creating the user model

--- DatingApp/DatingApp.API/Data/DataContext.cs
+++ DatingApp/DatingApp.API/Data/DataContext.cs
@@ -6,7 +6,9 @@ namespace DatingApp.API.Data
     public class DataContext : DbContext
     {
         public DbSet<Value> Values { get; set; }
-        
+
+        public DbSet<User> Users { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
