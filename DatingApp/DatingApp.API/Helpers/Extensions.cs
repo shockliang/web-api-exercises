289fd230fcbf405fd74e423d0bbb80731916f38e
Shock Liang <shockwater@gmail.com>
2017-12-26 23:03:40 +0800

    Fixing error handling headers typo

--- DatingApp/DatingApp.API/Helpers/Extensions.cs
+++ DatingApp/DatingApp.API/Helpers/Extensions.cs
@@ -7,8 +7,8 @@ namespace DatingApp.API.Helpers
         public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
-            response.Headers.Add("Access-Controll-Expose-Headers", "Application-Error");
-            response.Headers.Add("Access-Controll-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
     }
 }
