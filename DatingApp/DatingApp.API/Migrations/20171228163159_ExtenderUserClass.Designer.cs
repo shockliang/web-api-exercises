df6641c11f9a2357ba2066407c3ae870b4f5679c
Shock Liang <shockwater@gmail.com>
2017-12-29 00:35:36 +0800

    Extending the User model

--- DatingApp/DatingApp.API/Migrations/20171228163159_ExtenderUserClass.Designer.cs
+++ DatingApp/DatingApp.API/Migrations/20171228163159_ExtenderUserClass.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20171228163159_ExtenderUserClass")]
+    partial class ExtenderUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
