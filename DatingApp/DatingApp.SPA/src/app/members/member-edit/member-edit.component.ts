f5dd75eb7dfb14a04ec80c4ed82d2a43d5e3bf0f
Shock Liang <shockwater@gmail.com>
2018-01-03 21:33:48 +0800

    Creating the member edit template

--- DatingApp/DatingApp.SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp/DatingApp.SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,10 @@
+import { AlertifyService } from './../../services/alertify.service';
 import { User } from './../../models/User';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-member-edit',
@@ -9,13 +13,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  @ViewChild('editForm') editFrom: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService) { }
 
   ngOnInit() {
-    this.route.data.subscribe(data=> {
+    this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile Updated successfully');
+    this.editFrom.reset(this.user);
   }
 
 }
