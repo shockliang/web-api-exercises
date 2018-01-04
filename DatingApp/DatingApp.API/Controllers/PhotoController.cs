edaa16d438ea2c79ef03d8a06c6a1ab26d6e3b82
Shock Liang <shockwater@gmail.com>
2018-01-04 21:58:43 +0800

    Creating the photos controller

---
+++ DatingApp/DatingApp.API/Controllers/PhotoController.cs
@@ -0,0 +1,92 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    public class PhotoController : Controller
+    {
+        private readonly IDatingRepository repo;
+        private readonly IMapper mapper;
+        private readonly IOptions<CloudinarySettings> cloudinaryConfig;
+        private readonly Cloudinary cloudinary;
+
+        public PhotoController(
+            IDatingRepository repo,
+            IMapper mapper,
+            IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            this.cloudinaryConfig = cloudinaryConfig;
+            this.mapper = mapper;
+            this.repo = repo;
+
+            Account acc = new Account(
+                cloudinaryConfig.Value.CloudName,
+                cloudinaryConfig.Value.ApiKey,
+                cloudinaryConfig.Value.ApiSecret);
+
+            cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoDto)
+        {
+            var user = await repo.GetUser(userId);
+            if(user == null)
+                return BadRequest("Could not find user");
+
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            if(currentUserId != userId)
+                return Unauthorized();
+
+            var file = photoDto.File;
+
+            var uploadResult = new ImageUploadResult();
+
+            if(file.Length>0)
+            {
+                using(var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.Name, stream)
+                    };
+
+                    uploadResult = cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photoDto.Url = uploadResult.Uri.ToString();
+            photoDto.PublicId = uploadResult.PublicId;
+
+            var photo = mapper.Map<Photo>(photoDto);
+
+            if(!user.Photos.Any(m => m.IsMain))
+            {
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+
+            if(await repo.SaveAll())
+            {
+                return Ok();
+            }
+
+            return BadRequest("Could not add the photo");
+        }
+    }
+}
