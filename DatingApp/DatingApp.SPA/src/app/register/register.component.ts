04b264028a1b018c1f3ea30dacb83be1f60ef532
Shock Liang <shockwater@gmail.com>
2018-01-06 22:40:46 +0800

    Using the reactive forms FormBuilder service

--- DatingApp/DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp.SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -14,17 +14,24 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   registerForm: FormGroup
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [
-        Validators.required, 
-        Validators.minLength(4), 
-        Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', Validators.required)
-    }, this.passwrodMatchValidator);
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwrodMatchValidator })
   }
 
   passwrodMatchValidator(group: FormGroup) {
