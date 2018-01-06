479d640755a7813377791c3a20da35a2f0a3c29a
Shock Liang <shockwater@gmail.com>
2018-01-06 23:55:09 +0800

    Refactoring: Extract the error detection methods to reduce duplicate codes

--- DatingApp/DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp.SPA/src/app/register/register.component.ts
@@ -38,6 +38,30 @@ export class RegisterComponent implements OnInit {
     return group.get('password').value === group.get('confirmPassword').value ? null : { 'mismatch': true }
   }
 
+  hasAnyError(formControl: string): boolean {
+    return this.registerForm.get(formControl).errors && this.registerForm.get(formControl).touched;
+  }
+
+  hasRequiredError(formControl: string): boolean {
+    return this.hasSpecifyError(formControl, 'required');
+  }
+
+  hasMinLengthError(formControl: string): boolean {
+    return this.hasSpecifyError(formControl, 'minlength');
+  }
+  
+  hasMaxLengthError(formControl: string): boolean {
+    return this.hasSpecifyError(formControl, 'maxlength');
+  }
+
+  hasMismatchError(): boolean {
+    return this.registerForm.get('confirmPassword').touched && this.registerForm.hasError('mismatch')
+  }
+
+  hasSpecifyError(formControl: string, specifyErrorType: string) {
+    return this.registerForm.get(formControl).hasError(specifyErrorType) && this.registerForm.get(formControl).touched
+  }
+
   register() {
     // this.authService.register(this.model).subscribe(() => {
     //   this.alertify.success('Registration successful');
