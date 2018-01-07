ed05e1a8d884bd5d68b3a5cd98c37df12dc61845
Shock Liang <shockwater@gmail.com>
2018-01-07 15:50:47 +0800

    Completing the registration implementation

--- DatingApp/DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp.SPA/src/app/register/register.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { User } from './../models/User';
 import { AuthService } from './../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
@@ -12,20 +14,21 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
     this.bsConfig = {
       containerClass: 'theme-red'
     };
-    
+
     this.createRegisterForm();
   }
 
@@ -33,7 +36,7 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       gender: ['male'],
       username: ['', Validators.required],
-      knownAs: ['', Validators.required],
+      knowAs: ['', Validators.required],
       dateOfBirth: [null, Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
@@ -60,7 +63,7 @@ export class RegisterComponent implements OnInit {
   hasMinLengthError(formControl: string): boolean {
     return this.hasSpecifyError(formControl, 'minlength');
   }
-  
+
   hasMaxLengthError(formControl: string): boolean {
     return this.hasSpecifyError(formControl, 'maxlength');
   }
@@ -74,11 +77,19 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    // this.authService.register(this.model).subscribe(() => {
-    //   this.alertify.success('Registration successful');
-    // }, error => {
-    //   this.alertify.error(error);
-    // });
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Registration successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/members']);
+        })
+      });
+    }
   }
 
   cancel() {
