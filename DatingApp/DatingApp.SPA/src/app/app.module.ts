b0c30c2aacc00e93e8208b5a07000619f7255d11
Shock Liang <shockwater@gmail.com>
2018-01-07 16:31:20 +0800

    Using a TimeAgo pipe for dates in angular.

--- DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { NgxGalleryModule } from 'ngx-gallery';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 
 @NgModule({
@@ -45,7 +46,8 @@ import { FileUploadModule } from 'ng2-file-upload';
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
